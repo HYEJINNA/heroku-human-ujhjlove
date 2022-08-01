1fd3be12d4d0f9d6a0299caec13bbc1605e36993
HYEJINNA <ujhjlove1@gmail.com>
2022-08-01 15:17:48 +0900

    update

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_remplate
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_remplate('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(port=5000)
